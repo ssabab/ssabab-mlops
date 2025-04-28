3707f5e74294f082d04a63f259b93cc4d3f0ca08
hdh026163@gmail.com <hdh026163@gmail.com>
2025-04-28 16:23:49 +0900

    feat: SSABAB 레거시 시스템용 Airflow DAG 및 Task 기본 구조 추가

---
+++ dags/tasks/menu.py
@@ -0,0 +1,11 @@
+from airflow.decorators import task
+
+@task(task_id="validate_menu")
+def validate():
+    
+    return
+
+@task(task_id="preprocessing_menu")
+def preprocessing():
+
+    return
---
+++ dags/tasks/vote.py
@@ -0,0 +1,11 @@
+from airflow.decorators import task
+
+@task(task_id="validate_vote")
+def validate():
+    
+    return
+
+@task(task_id="preprocessing_vote")
+def preprocessing():
+
+    return
